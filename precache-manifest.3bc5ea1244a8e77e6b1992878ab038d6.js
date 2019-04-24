304eac6cae682d864c5c7b3dbd33648cf64178b1
frnkly <frank@frnk.ca>
2019-04-24 18:54:31 -0400

    Deployed to Github pages

---
+++ precache-manifest.3bc5ea1244a8e77e6b1992878ab038d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb5b29cd45c04be57edc2e57bc67a05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a78d6860905af85f11a",
+    "url": "/static/css/main.96137cdc.chunk.css"
+  },
+  {
+    "revision": "f8348ac27a7d6ee001b3",
+    "url": "/static/js/2.f1c924c2.chunk.js"
+  },
+  {
+    "revision": "3a78d6860905af85f11a",
+    "url": "/static/js/main.9ef90907.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
